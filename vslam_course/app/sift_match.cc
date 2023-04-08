d79d960a4e18410359ec2ef32376bf730a89988d
bruce <zsyuyizhang@gmail.com>
2023-04-08 23:16:28 +0800

    add sift match main
    Signed-off-by: bruce <zsyuyizhang@gmail.com>

---
+++ vslam_course/app/sift_match.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+
+#include "feature/sift.h"
+#include "common/string_util.h"
+#include "common/image_util.h"
+
+namespace zs {
+
+class SiftMatch {
+  public:
+    SiftMatch(const std::string data_folder): data_folder_(data_folder) {}
+    ~SiftMatch() {}
+
+    void run();
+
+  private:
+    std::string data_folder_;
+};
+
+void SiftMatch::run() {
+    try {
+        zs::Sift sift(200, 5, 3, 0.04, 10, 1.6);
+        cv::Mat image = cv::imread(data_folder_ + "image1.jpg",cv::IMREAD_GRAYSCALE);
+        cv::Mat image2 = cv::imread(data_folder_ + "image2.jpg",cv::IMREAD_GRAYSCALE);
+        
+        std:::vector<cv::KeyPoint> kp_ref;
+        std::vector<cv::vector<float>> desc_ref;
+
+        sfit.detect(image, kp_ref, desc_ref);
+        sift.plotGaussignPyramid("./output/gaussian_pyr_ref.png", true);
+        sift.plotDogPyramid("./output/dog_pyr_ref.png", true);
+        sfit.plotKeypoints(image,  kp_ref,"./output/keypoints_ref.png", true);
+
+        std::vector<cv::KeyPoint> kp_query;
+        std::vector<std::vector<float>> desc_query;
+        sift.detect(image2, kp_query, desc_query);
+        sift.plotGaussignPyramid("./output/gaussian_pyr_query.png", true);
+        sift.plotDogPyramid("./output/dog_pyr_query.png", true);
+        sift.plotKeypoints(image2, kp_query ,"./output/keypoints_query.png", true);
+
+        std::vector<int> match_idx;
+        sift.match(desc_ref, desc_query, match_idx);
+        sft.plotMatchTwoImage(image1, image2, kp_ref, kp_query, match_idx, "./output/match_two_image.png", true);
+
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    
+}
+
+}
+
+int main(int argc, char **argv) {
+    if (argc!= 2) {
+        std::cout << "Usage: " << argv[0] << " data_folder" << std::endl;
+        return 0;
+    }
+
+    std::string data_folder = zs::folder_and_slash( argv[1]);
+    zs::SiftMatch match(data_folder);
+    match.run();
+    return 0;
+}
